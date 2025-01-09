5e37283d8e7a950667b243f236c0ae0dd2673faa
Momrad <adamgames2003@gmail.com>
2025-01-09 15:15:15 +0100

    Added base endpoints

--- distractible-backend/src/databaseManager.js
+++ distractible-backend/src/databaseManager.js
@@ -1,48 +1,133 @@
-const db = require('mongoose');
-require('dotenv').config();
+const { ListSearchIndexesCursor, ObjectId } = require("mongodb");
+const db = require("mongoose");
+require("dotenv").config();
 
-const point = db.Schema({
-    point : {type : Number, required : true},
-    date : {type : Date, required : true},
-    competitor : {type : String, required : true},
-    description : {type : String, required : true}
+const pointSchema = db.Schema({
+  point: { type: Number, required: true },
+  date: { type: Date, required: true },
+  competitor: { type: String, required: true },
+  description: { type: String, required: true },
 });
 
-const episode = new db.Schema({
-    date : {type : Date, required : true},
-    title: {type : String},
-    points : {type : Array, "default" : []},
-    host : [point]
+const episodeSchema = new db.Schema({
+  date: { type: Date, required: true },
+  title: { type: String },
+  host: { type: String, required: true },
+  points: [pointSchema],
+  winner: { type: String },
+  competitors: { type: Array, required: true },
 });
 
 async function connectDatabase() {
-    try {
-        await db.connect(process.env.DB_URL);
-        console.log('Database connected successfully!');
-    } catch (error) {
-        console.error('Error connecting to the database:', error);
-        process.exit(1); // Exit process with failure
-    }
+  try {
+    await db.connect(process.env.DB_URL);
+    console.log("Database connected successfully!");
+  } catch (error) {
+    console.error("Error connecting to the database:", error);
+    process.exit(1); // Exit process with failure
+  }
 }
 
 module.exports = {
-    connectDatabase,
-    getTodaysEpisode,
-    addPoint,
-    createEpisode
+  connectDatabase,
+  getEpisodeByDate,
+  addPoint,
+  createEpisode,
 };
 
-async function getTodaysEpisode(){
-    //This function will return today's episode's information. If there wasnt an episode today, it will return null
+const Episode = db.model("Episode", episodeSchema);
+const Point = db.model("Point", pointSchema);
+
+async function getEpisodeByDate(date) {
+  try {
+    console.log("Trying to query episde by date: " + date);
+
+    const result = await await Episode.find().where({ date: new Date(date) });
+
+    console.log("Search result: " + result);
+
+    if (result != null) {
+      return result;
+    } else {
+      return {
+        error:
+          "Can't find episode with the provided date! Check if the format or the date is right!",
+      };
+    }
+  } catch (error) {
+    console.log(error);
+    return {
+      error:
+        "Something went wrong! Please check if request was submitted with the right data, in the right format!",
+    };
+  }
 }
 
-async function addPoint(date, point, description, competitor){
-    //This function will insert a point to the episode by date. It will return an error if there is episode with that date
-    //it will return the id of the point
+async function addPoint(point, episodeId) {
+  console.log("Trying to new point into episode: " + episodeId);
+
+  try {
+    if (
+      !point.point ||
+      !point.date ||
+      !point.description ||
+      !point.competitor
+    ) {
+      return { error: "One or mode fields are missing!" };
+    }
+
+    const searcedEpisode = await Episode.findById(episodeId);
+
+    if (searcedEpisode) {
+      const newPoint = new Point({
+        point: point.point,
+        date: new Date(point.date),
+        description: point.date,
+        competitor: point.description,
+      });
+      searcedEpisode.points.push(newPoint);
+      const updatedEpisode = await searcedEpisode.save();
+
+      return updatedEpisode._id;
+    } else {
+      console.log("Episode " + episodeId + " was not found!");
+      return { error: "Episode was not found" };
+    }
+  } catch (err) {
+    console.log(error);
+    return {
+      error:
+        "Something went wrong! Please check if request was submitted with the right data, in the right format!",
+    };
+  }
 }
 
-async function createEpisode(date, host, competitors, title = null){
-    //this function will create a record of a new episode, from date, host, an array of competitors, and an optional title.
-    //It returns an error if an episode already exists on that date
-    //it returns the id of the new episode
-} 
+async function createEpisode(date, host, competitors, title = null) {
+  try {
+    console.log("Trying to fetch episode with date: " + date);
+
+    const searchResult = await Episode.find().where({ date: date });
+
+    if (searchResult.length < 1) {
+      console.log("Episode not have been found! Trying to create new episode!");
+
+      const newEpisode = new Episode({
+        date: date,
+        host: host,
+        competitors: competitors,
+        title: title,
+      });
+      newEpisode.save();
+      console.log("New episode have been created! id: " + newEpisode._id);
+      return await Episode.findOne({ date: date });
+    } else {
+      return { error: "An episode for this date already exists!" };
+    }
+  } catch (error) {
+    console.log(error);
+    return {
+      error:
+        "Something went wrong! Please check if request was submitted with the right data, in the right format!",
+    };
+  }
+}
--- distractible-backend/src/index.js
+++ distractible-backend/src/index.js
@@ -12,10 +12,65 @@ const dataBase = require("./databaseManager");
   }
 })();
 
+app.use(express.json());
+
 app.get('/', (req, res) => {
   res.send('Yes, im working!')
 })
 
 app.listen(port, () => {
   console.log(`The server is listening on port ${port}`)
+})
+
+app.post('/api/create-episode', async (req, res) => {
+
+  const host = req.body.host;
+  const date = req.body.date;
+  const title = req.body.title;
+  const competitors = req.body.competitors;
+
+  const response = await dataBase.createEpisode(date, host, competitors, title);
+
+  if(!response.error){
+    res.status(201);
+    res.send(response);
+  }
+  else{
+    res.status(409);
+    res.send(response.error);
+  }
+})
+
+app.get('/api/get-episode-by-date', async (req, res) => {
+
+  //TODO validate date!
+
+  const date = req.body.date;
+  const result = await dataBase.getEpisodeByDate(date);
+
+  if(!result.error){
+    res.status(200);
+    res.send(result);
+  }
+  else{
+    res.status(404);
+    res.send(result.error);
+  }
+})
+
+app.post('/api/add-point-to-episode', async(req, res) => {
+
+    const point = req.body.point
+    const episodeId = req.body.episodeId;
+
+    const result = await dataBase.addPoint(point, episodeId);
+
+    if(result.error){
+      res.status(406);
+      res.send(result.error);
+    }
+    else{
+      res.status(200);
+      res.send(result.error);
+    }
 })
