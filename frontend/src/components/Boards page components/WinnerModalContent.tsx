d23d681308acfc99ca42af764b5a8f31d049f16f
Momrad <adamgames2003@gmail.com>
2025-03-17 16:22:24 +0100

    added points counter to the winner modal

--- frontend/src/components/Boards page components/WinnerModalContent.tsx
+++ frontend/src/components/Boards page components/WinnerModalContent.tsx
@@ -9,8 +9,9 @@ export default function WinnerModalContent({competitor, onClose, onEndEpisode}){
                 <img className="w-3xs rounded-box" src={competitor.getCompetitorImage()} alt="" />
             </div>
             <h2 className="text-center text-4xl">{competitor.getName()}</h2>
+            <h2 className="text-center text-1xl">With {competitor.tabulatePoints()} points!</h2>
             <div className="flex gap-4">
-                <button className="btn btn-soft flex-1">Cancel</button>
+                <button onClick={onClose} className="btn btn-soft flex-1">Cancel</button>
                 <button className="btn btn-soft btn-success flex-1">End Episode</button>
             </div>
         </div>
