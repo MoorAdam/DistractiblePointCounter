2b9289dd382e56cfcfa497ebb69cca0215bc8602
Momrad <adamgames2003@gmail.com>
2025-03-19 13:14:12 +0100

    changed endEpisodeFeilds to be universal, and changed it to submit the edited states instead of editing the main states

--- frontend/src/components/Boards page components/EndEpisodeFields.tsx
+++ frontend/src/components/Boards page components/EndEpisodeFields.tsx
@@ -1,7 +1,15 @@
 import React from "react";
+import {useState} from "react"
+import {Episode} from "@types";
 
-export default function EndEpisodeFields({recordingDate, setRecordingDate, releaseDate, setReleaseDate, host, setHost, winner = null, setWinner = null, title, setTitle, onEndEpisode, onCancel}) {
+export default function EndEpisodeFields({recordingDate = null, setRecordingDate, releaseDate = null, setReleaseDate, host = null, setHost, winner = null, setWinner = null, title = null, setTitle, onSubmit, submitLabel, onCancel}) {
     
+    const [localTitle, setLocalTitle] = useState<string>(title);
+    const [localRecordingDate, setLocalRecordingDate] = useState<Date>(recordingDate);
+    const [localReleaseDate, setLocalReleaseDate] = useState<Date>(releaseDate);
+    const [localHost, setLocalHost] = useState<string>(host ? host.getName() : null);
+    const [localWinner, setLocalWinner] = useState<string>(winner ? winner.getName() : null);
+
     const getYMD = (date) => {
         const year = date.getFullYear();
         const month = String(date.getMonth() + 1).padStart(2, '0');
@@ -10,31 +18,49 @@ export default function EndEpisodeFields({recordingDate, setRecordingDate, relea
         return `${year}-${month}-${day}`;
     }
 
-    console.log(JSON.stringify(host))
+    function onSubmitting(){
+        setTitle(localTitle);
+        setHost(localHost);
+        setRecordingDate(localRecordingDate);
+        releaseDate ? setReleaseDate(localReleaseDate) : "";
+        winner ? setWinner(localWinner) : ""
+
+        const newEpisodeDetails : Episode = {
+            title : localTitle ? localTitle : null,
+            recordingDate : localRecordingDate,
+            releaseDate : localReleaseDate ? localReleaseDate : null,
+            host : localHost,
+            winner : localWinner ? localWinner : null,
+            competitors : ["Mark", "Bob", "Wade"]
+        }
+
+
+        onSubmit(newEpisodeDetails);
+    }
     
     return (
         <div className="flex flex-col">
             <label className="text-sm font-semibold text-gray-400">Episode title (You can set it on the Episodes page)</label>
-            <input type="text" placeholder={title ? title : ""} onChange={(e) => setTitle(e.target.value)} className="border border-gray-300 p-2 mt-1" />
+            <input type="text" placeholder={localTitle ? localTitle : ""} onChange={(e) => setLocalTitle(e.target.value)} className="border border-gray-300 p-2 mt-1" />
 
             <label className="text-sm font-semibold text-gray-400 mt-4">Recording date</label>
-            <input type="date" value={recordingDate ? getYMD(recordingDate) : ""} onChange={(e) => setRecordingDate(new Date(e.target.value))} className="border border-gray-300 p-2 mt-1" />
+            <input type="date" value={localRecordingDate ? getYMD(localRecordingDate) : getYMD(new Date(Date.now()))} onChange={(e) => setLocalRecordingDate(new Date(e.target.value))} className="border border-gray-300 p-2 mt-1" />
 
             <label className="text-sm font-semibold text-gray-400 mt-4">Release date (You can set it on the Episodes page)</label>
-            <input type="date" value={recordingDate ? getYMD(releaseDate) : ""} onChange={(e) => setReleaseDate(new Date(e.target.value))} className="border border-gray-300 p-2 mt-1" />
+            <input type="date" value={localReleaseDate ? getYMD(localReleaseDate) : ""} onChange={(e) => setLocalReleaseDate(new Date(e.target.value))} className="border border-gray-300 p-2 mt-1" />
 
             <label className="text-sm font-semibold text-gray-400 mt-4">Host</label>
-            <select onChange={(e) => setHost(e.target.value)} defaultValue={host} className="border border-gray-300 w-full p-2 mt-1 select">
+            <select required onChange={(e) => setLocalHost(e.target.value)} defaultValue={localHost ? localHost : "Mark"} className="border border-gray-300 w-full p-2 mt-1 select">
                 <option value="Mark">Mark</option>
                 <option value="Bob">Bob</option>
                 <option value="Wade">Wade</option>
             </select>
 
-            {host ? 
+            {winner ? 
                 (
                     <>
                         <label className="text-sm font-semibold text-gray-400 mt-4">Winner</label>
-                        <select onChange={(e) => setWinner(e.target.value)} defaultValue={winner.getName()} className="border border-gray-300 w-full p-2 mt-1 select">
+                        <select onChange={(e) => setLocalWinner(e.target.value)} defaultValue={localWinner} className="border border-gray-300 w-full p-2 mt-1 select">
                             <option value="Mark">Mark</option>
                             <option value="Bob">Bob</option>
                             <option value="Wade">Wade</option>
@@ -43,7 +69,7 @@ export default function EndEpisodeFields({recordingDate, setRecordingDate, relea
                 
                 ) : ""} 
 
-            <button onClick={onEndEpisode} className="bg-blue-500 text-white p-2 mt-4">End Episode</button>
+            <button onClick={onSubmitting} className="bg-blue-500 text-white p-2 mt-4">{submitLabel}</button>
             <button onClick={onCancel} className="bg-black-500 text-white p-2 mt-4">Cancel</button>
         </div>
     );
