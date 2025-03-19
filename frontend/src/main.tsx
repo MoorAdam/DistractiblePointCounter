c7682fa100a98f057cffac1094aff296bfb603a0
Momrad <adamgames2003@gmail.com>
2025-03-19 14:11:02 +0100

    moved Boards to router

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.js'
+import './App.css'
 import './index.css'
+import { BrowserRouter, Routes, Route } from "react-router";
+import Boards from "./pages/Boards.js"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Boards/>}/>
+    </Routes>
+  </BrowserRouter>
 )
